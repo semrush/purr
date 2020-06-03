70e1520134616c92ebea73f33db9a1025cb0c4aa
Vyacheslav Artemiev <v.artemiev@semrush.com>
2020-06-03 22:36:38 +0300

    Fix cli-check return code
    Exit with code 1 when check complete but not success
    Refactor tests

---
+++ src/cli/check.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+const util = require('util');
+
+const Logger = require('../Logger');
+const SimpleQueue = require('../queue/SimpleQueue');
+const CheckRunner = require('../check/runner');
+
+function run(checkName) {
+  const log = new Logger();
+  const checkRunner = new CheckRunner(new SimpleQueue());
+
+  log.info(`Running check ${JSON.stringify(checkName)}`);
+
+  return checkRunner
+    .run(checkName)
+    .then((result) => {
+      if (!result.success) {
+        log.error(
+          'Check failed\n',
+          util.inspect(result, {
+            colors: true,
+            depth: null,
+            maxArrayLength: null,
+          })
+        );
+        process.exit(1);
+      }
+
+      log.info(
+        'Check success\n',
+        util.inspect(result, {
+          colors: true,
+          depth: null,
+          maxArrayLength: null,
+        })
+      );
+    })
+    .catch((err) => {
+      log.error(
+        'Check failed\n',
+        util.inspect(err, { colors: true, depth: null, maxArrayLength: null })
+      );
+      process.exit(1);
+    });
+}
+
+module.exports = { run };
