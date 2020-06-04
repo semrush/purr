39ef3db0800429d373161d3dd44ac5e2cd13398d
Vyacheslav Artemiev <v.artemiev@semrush.com>
2020-06-04 23:20:26 +0300

    Update uuid package

--- src/api/views/Checks.js
+++ src/api/views/Checks.js
@@ -1,4 +1,4 @@
-const uuidv4 = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
 
 const Logger = require('../../Logger');
 const config = require('../../config');
--- src/schedule/runner.js
+++ src/schedule/runner.js
@@ -1,4 +1,4 @@
-const uuidv4 = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
 const Redis = require('ioredis');
 
 const config = require('../config');
