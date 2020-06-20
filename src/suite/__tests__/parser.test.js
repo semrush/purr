94c6c8f22471d71322c3c2090331ff2114050bfb
Vyacheslav Artemiev <v.artemiev@semrush.com>
2020-06-20 06:18:56 +0300

    Update tests

--- src/suite/__tests__/parser.test.js
+++ src/suite/__tests__/parser.test.js
@@ -10,7 +10,23 @@ beforeEach(async () => {
 });
 
 describe('SuiteParser', () => {
-  test('get suite', () => {
+  test('get list', () => {
+    expect(parser.getList()).toEqual([
+      'mocked-suite',
+      'failing-mocked-suite',
+      'mocked-suite-with-exception',
+      'empty-suite',
+      'empty-steps-suite',
+    ]);
+  });
+
+  test('fail when name argument is not specified', () => {
+    expect(() => {
+      parser.getSuite();
+    }).toThrow("Mandatory parameter 'name' is missing");
+  });
+
+  test('getSuiteSteps', () => {
     expect(parser.getSuiteSteps('mocked-suite')).toEqual([
       'mocked-check',
       'mocked-check-with-param',
@@ -23,9 +39,35 @@ describe('SuiteParser', () => {
     }).toThrow("Suite with name 'unknown-suite' does not exist");
   });
 
+  test('fail when trying to get empty suite', () => {
+    expect(() => {
+      parser.getSuiteSteps('empty-suite');
+    }).toThrow("Suite with name 'empty-suite' is empty");
+  });
+
+  test('fail when trying to get suite without steps', () => {
+    expect(() => {
+      parser.getSuiteSteps('empty-steps-suite');
+    }).toThrow("Suite with name 'empty-steps-suite' has no steps");
+  });
+
   test('fail when name argument is not specified', () => {
     expect(() => {
       parser.getSuiteSteps();
+    }).toThrow("Mandatory parameter 'name' is missing");
+  });
+
+  test('proxy is null by default', () => {
+    expect(parser.getSuiteProxy('empty-steps-suite')).toBe(null);
+  });
+
+  test('getSuiteProxy', () => {
+    expect(parser.getSuiteProxy('mocked-suite')).toBe('some_url');
+  });
+
+  test('fail when name argument is not specified', () => {
+    expect(() => {
+      parser.getSuiteProxy();
     }).toThrow("Mandatory parameter 'name' is missing");
   });
 });
--- src/suite/__tests__/runner.test.js
+++ src/suite/__tests__/runner.test.js
@@ -9,42 +9,48 @@ test('successful suite', () => {
   const queue = new SimpleQueue();
   const runner = new SuiteRunner(queue);
 
-  return runner.run('mocked-suite', Math.random()).then((data) => {
+  return runner.run('mocked-suite').then((data) => {
     expect(data).toBeInstanceOf(SuiteReport);
     expect(data.success).toEqual(true);
     expect(data.checks[0].name).toEqual('mocked-check');
   });
 });
 
-test('failing suite', () => {
-  const queue = new SimpleQueue();
-  const runner = new SuiteRunner(queue);
+test('failing suite', async () => {
+  const runner = new SuiteRunner();
 
-  return runner.run('failing-mocked-suite', Math.random()).then((data) => {
-    expect(data).toBeInstanceOf(SuiteReport);
-    expect(data.success).toEqual(false);
-  });
+  const data = await runner.run('failing-mocked-suite', Math.random());
+
+  expect(data).toBeInstanceOf(SuiteReport);
+  expect(data.success).toEqual(false);
+  expect(data.checks[0].success).toEqual(true);
+  expect(data.checks[1].success).toEqual(false);
 });
 
 test('suite with exception', () => {
-  const queue = new SimpleQueue();
-  const runner = new SuiteRunner(queue);
+  const runner = new SuiteRunner();
 
   return runner
     .run('mocked-suite-with-exception', Math.random())
     .then((data) => {
       expect(data).toBeInstanceOf(SuiteReport);
       expect(data.success).toEqual(false);
+      expect(data.checks[0].success).toEqual(true);
+      expect(data.checks[1]).toBeInstanceOf(Error);
     });
 });
 
 test('unknown suite', async () => {
-  const queue = new SimpleQueue();
-  const runner = new SuiteRunner(queue);
+  const runner = new SuiteRunner();
 
   await expect(
     runner.run('unexisted-mocked-suite', Math.random())
   ).rejects.toThrowError(
     "Suite with name 'unexisted-mocked-suite' does not exist"
   );
+});
+
+test('fail when name argument is not specified', () => {
+  const runner = new SuiteRunner();
+  expect(runner.run()).rejects.toThrow("Mandatory parameter 'name' is missing");
 });
